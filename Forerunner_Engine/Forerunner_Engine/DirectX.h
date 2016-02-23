92e808fea1c96719f656b9904921f1624b71230d
Haval-Ahmed <Haval_Sadik_Ahmed@outlook.com>
2016-02-22 20:12:00 -0800

    Adds initial file basis for Engine.

---
+++ Forerunner_Engine/Forerunner_Engine/DirectX.h
@@ -0,0 +1,22 @@
+/*
+Creator: Haval Ahmed
+Notice: All Code is © Haval Ahmed
+Description: DirectX Rendering Header File for Forerunner Engine
+*/
+
+/*
+Class: DirectX_Rendering
+Info: The Base Class for all DirectX Rendering Functions. This will include the rendering, updating, device creation, statistics, and more.
+*/
+
+
+#pragma once
+
+
+class DirectX
+{
+public:
+	DirectX();
+	virtual ~DirectX();
+};
+
---
+++ Forerunner_Engine/Forerunner_Engine/WinMain.cpp
@@ -0,0 +1,5 @@
+/*
+Creator: Haval Ahmed 
+Notice: All Code is © Haval Ahmed
+Description: Win Main Initialization for Forerunner Engine
+*/
